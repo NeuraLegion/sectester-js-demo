b9296b59fd25274374b9f5aeac5046d5842e00e7
Artem Derevnjuk <artem.derevnjuk@neuralegion.com>
2022-05-31 14:45:28 +0400

    build(jest): set env vars on global setup

--- test/global-setup.js
+++ test/global-setup.js
@@ -1,5 +1,6 @@
 const detectPort = require('detect-port');
 const dockerCompose = require('docker-compose');
+const { config } = require('dotenv');
 const { join } = require('path');
 const { promisify } = require('util');
 const { exec } = require('child_process');
@@ -8,6 +9,8 @@ module.exports = async () => {
   const port = 5432;
   const freePort = await promisify(detectPort)(port);
   const cwd = join(__dirname, '..');
+
+  config();
 
   if (freePort === port) {
     await dockerCompose.upAll({
