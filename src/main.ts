d1c70937598c264a01850a409ae96e9fa3ebf4c5
Artem Derevnjuk <artem.derevnjuk@neuralegion.com>
2022-05-31 16:10:53 +0400

    docs(readme): describe how to explore the demo application

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 import { logger } from '@mikro-orm/nestjs';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
+
   app.enableShutdownHooks();
+
+  const config = new DocumentBuilder()
+    .setTitle('SecTester JS Demo')
+    .setDescription(
+      'This is a demo project for the SecTester JS SDK framework, with some installation and usage examples.'
+    )
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
   logger.log(`Application is running on: ${await app.getUrl()}`);
 };
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,27 +10,40 @@ import {
   Post,
   Query
 } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('users')
+@ApiTags('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @ApiResponse({
+    status: 201,
+    type: User,
+    description: 'The record has been successfully created.'
+  })
   public create(@Body() createUserDto: CreateUserDto): Promise<User> {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
+  @ApiResponse({ status: 200, type: User, isArray: true })
   public findAll(@Query() query?: Omit<Partial<User>, 'id'>): Promise<User[]> {
     return this.usersService.findAll(query);
   }
 
   @Get(':id')
+  @ApiResponse({ status: 200, type: User })
   public findOne(@Param('id') id: number): Promise<User | null> {
     return this.usersService.findOne(id);
   }
 
   @Delete(':id')
+  @ApiResponse({
+    status: 204,
+    description: 'The record has been successfully removed.'
+  })
   public remove(@Param('id') id: number): Promise<void> {
     return this.usersService.remove(id);
   }
